4d505ad77837c929dd2488b0f83344e48f75c764
Henrique Marlon <henriquemarloncs@gmail.com>
2023-09-29 01:42:32 -0300

    feat: add gist to home page

---
+++ lilium/components/Gist.jsx
@@ -0,0 +1,20 @@
+// components/Gist.js
+function Gist() {
+    return (
+      <div className="gist-container">
+        <iframe 
+          src="https://gist.github.com/henriquemarlon/6ed36a81b1507d977cacb1870abacc18.pibb" 
+          width="100%">
+        </iframe>
+        <style jsx>{`
+          iframe {
+            height: 80vh;
+            width: 100%; 
+          }
+        `}</style>
+      </div>
+    );
+  }
+  
+  export default Gist;
+  
