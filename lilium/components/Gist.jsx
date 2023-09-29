2d1ac38504780b167b8edda9fc3ac39e3281502f
Henrique Marlon <henriquemarloncs@gmail.com>
2023-09-29 05:19:52 -0300

    feat: some changes

--- lilium/components/Gist.jsx
+++ lilium/components/Gist.jsx
@@ -3,7 +3,7 @@ function Gist() {
     return (
       <div className="gist-container">
         <iframe 
-          src="https://gist.github.com/henriquemarlon/6ed36a81b1507d977cacb1870abacc18.pibb-" 
+          src="https://gist.github.com/henriquemarlon/6ed36a81b1507d977cacb1870abacc18.pibb" 
           width="100%">
         </iframe>
         <style jsx>{`
